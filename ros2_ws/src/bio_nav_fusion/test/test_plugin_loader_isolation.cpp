bdf52f83123fb2fad94b272f7d41097d3a9f40d9
AoiOTA <liang_yibo@hdu.edu.cn>
2026-07-31 06:16:04 +0800

    fix: isolate risk and planner plugin libraries

---
+++ ros2_ws/src/bio_nav_fusion/test/test_plugin_loader_isolation.cpp
@@ -0,0 +1,21 @@
+#include <memory>
+
+#include "gtest/gtest.h"
+#include "nav2_core/global_planner.hpp"
+#include "nav2_costmap_2d/layer.hpp"
+#include "pluginlib/class_loader.hpp"
+
+TEST(BioNavFusionPlugins, risk_layer_does_not_preload_stock_smac_factory)
+{
+  pluginlib::ClassLoader<nav2_costmap_2d::Layer> costmap_loader(
+    "nav2_costmap_2d", "nav2_costmap_2d::Layer");
+  const auto risk_layer = costmap_loader.createUniqueInstance(
+    "bio_nav_fusion::CognitiveRiskLayer");
+  ASSERT_NE(risk_layer, nullptr);
+
+  pluginlib::ClassLoader<nav2_core::GlobalPlanner> planner_loader(
+    "nav2_core", "nav2_core::GlobalPlanner");
+  const auto stock_planner = planner_loader.createUniqueInstance(
+    "nav2_smac_planner::SmacPlanner2D");
+  EXPECT_NE(stock_planner, nullptr);
+}
