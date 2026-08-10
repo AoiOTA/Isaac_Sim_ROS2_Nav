9832aa5bff52fb8135ec34882d121a63a893b375
AoiOTA <liang_yibo@hdu.edu.cn>
2026-08-10 19:49:38 +0800

    perf(attempt28): disable D1 trajectory visualization

--- ros2_ws/src/robot_navigation/test/test_nav2_config.py
+++ ros2_ws/src/robot_navigation/test/test_nav2_config.py
@@ -312,6 +312,7 @@ def test_attempt28_a1_and_a19_profiles_are_explicitly_isolated():
             assert 'FollowPath' not in parameters
         elif batch == 'bounded_portal':
             follow_path = parameters['FollowPath']
+            assert follow_path['visualize'] is False
             assert follow_path['max_robot_pose_search_dist'] == 0.21
             assert follow_path['PathAngleCritic']['mode'] == 2
             assert follow_path['PathAngleCritic'][
