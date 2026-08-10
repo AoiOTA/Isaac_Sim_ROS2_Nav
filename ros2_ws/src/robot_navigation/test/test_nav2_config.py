3d17a1389224af67a5efa6bee84587c8e89ebaa8
AoiOTA <liang_yibo@hdu.edu.cn>
2026-08-10 09:27:06 +0800

    attempt28: strengthen bounded forward tie break

--- ros2_ws/src/robot_navigation/test/test_nav2_config.py
+++ ros2_ws/src/robot_navigation/test/test_nav2_config.py
@@ -312,7 +312,7 @@ def test_attempt28_a1_and_a19_profiles_are_explicitly_isolated():
             assert follow_path['PathAngleCritic']['mode'] == 1
             assert follow_path['PreferForwardCritic'] == {
                 'enabled': True,
-                'cost_weight': 2.0,
+                'cost_weight': 6.0,
                 'threshold_to_consider': 0.20,
             }
             assert follow_path['vx_std'] == 0.35
